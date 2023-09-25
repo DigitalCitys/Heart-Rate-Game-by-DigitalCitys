9dc8c974d819e4a14ea5dd25218191d8ca72b333
Giulia Spier <gls1904@rit.edu>
2023-09-25 14:01:42 -0400

    Developing AED update

--- Heart Rate Game/heart_rate_game.py
+++ Heart Rate Game/heart_rate_game.py
@@ -2,12 +2,27 @@
 
 #Creates vitals for simulated comatose person
 
-def plug(bpm): #Plugs a comatosed patient
-    return bpm
+ABNORMAL_SHIFT = 25
+
+def plug(avg_bpm, bpm): #Plugs a comatosed patient
+    return avg_bpm, bpm
+
+def aed():
+    if bpm >= avg_bpm + ABNORMAL_SHIFT or bpm <= avg_bpm - ABNORMAL_SHIFT:
+        bpm = avg_bpm
 
 def main():
-    bpm = input("Enter heart rate: ")
-    print(plug(bpm))
+    avg_bpm = input("Enter average heart rate: ")
+    bpm = input("Enter current heart rate: ")
+    print("Average bpm: "), avg_bpm
+    print("Current bpm: "), bpm
+
+    if bpm >= avg_bpm + ABNORMAL_SHIFT or bpm <= avg_bpm - ABNORMAL_SHIFT:
+        aed_request = input("Heart rate abnormal. Administer defibrillator? YES or NO")
+        if aed_request == "YES":
+            aed()
+        else:
+            print("This could be life threatening!")
 
 main()
 
